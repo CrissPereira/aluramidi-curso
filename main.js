6d45fd19c782a689ebf7e62ce6e472e051c16cea
Cristian Pereira <cristianpereira2v@gmail.com>
2025-01-03 18:16:06 -0300

    Melhorando o código
    Usando laço de repetição para deixar o código mais simples, através de uma lista.

--- main.js
+++ main.js
@@ -1,64 +1,19 @@
-//Tecla Pom //
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const ListaDeTeclas = document.querySelectorAll('.tecla');
 
-//Tecla Clap //
-function tocaSomClap () {
-    document.querySelector('#som_tecla_clap').play();
+//equanto
+for( let contador = 0 ; contador < ListaDeTeclas.length; contador++){
+
+    const tecla = ListaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio= `#som_${instrumento}`;
+    
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
 }
-
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
-
-//Tecla Tim //
-function tocaSomTim () {
-    document.querySelector('#som_tecla_tim').play();
-}
-
-document.querySelector('.tecla_tim').onclick = tocaSomTim;
-
-//Tecla Puff //
-function tocaSomPuff () {
-    document.querySelector('#som_tecla_puff').play();
-}
-
-document.querySelector('.tecla_puff').onclick = tocaSomPuff;
-
-//Tecla Splash //
-function tocaSomSplash () {
-    document.querySelector('#som_tecla_splash').play();
-}
-
-document.querySelector('.tecla_splash').onclick = tocaSomSplash;
-
-//Tecla Toim //
-function tocaSomToim () {
-    document.querySelector('#som_tecla_toim').play();
-}
-
-document.querySelector('.tecla_toim').onclick = tocaSomToim;
-
-//Tecla Psh //
-function tocaSomPsh () {
-    document.querySelector('#som_tecla_psh').play();
-}
-
-document.querySelector('.tecla_psh').onclick = tocaSomPsh;
-
-//Tecla Tic //
-function tocaSomTic () {
-    document.querySelector('#som_tecla_tic').play();
-}
-
-document.querySelector('.tecla_tic').onclick = tocaSomTic;
-
-//Tecla Tom //
-function tocaSomTom () {
-    document.querySelector('#som_tecla_tom').play();
-}
-
-document.querySelector('.tecla_tom').onclick = tocaSomTom;
-
-
